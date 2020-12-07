0376e74b8f2315dc8479c99dc78d719f86dd410e
doktorat_dariusza_izaka <73701512+dariusz-izak-doktorat@users.noreply.github.com>
2020-12-07 11:54:39 +0100

    Test.sh
    Test of software

---
+++ test.sh
@@ -0,0 +1,4 @@
+$1 prwlr.py species_list  list_apoptotic_genes test
+$1 prwlr_ko.py species_list  list_apoptotic_genes test1
+diff test.tsv test/test.tsv | wc | awk '$1==0 {print "prwlr.py properly intalled"}'
+diff test.tsv test/test.tsv | wc | awk '$1==0 {print "prwlr_ko.py properly intalled"}'
